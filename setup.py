4e186d068d005e1ea1f03992fb337c8a9606bead
fajari <m.soleh.fajari@gmail.com>
2022-11-19 22:24:11 +0700

    Update new version and readme.md

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
 
     name="IndonesiaLatestEarthQuakeAlert",
-    version="0.0.3",
+    version="0.0.4",
     author="M Soleh Fajari",
     author_email="<m.soleh.fajari@gmail.com>",
     description="This package will guide us get earth quake information from bmkg",
