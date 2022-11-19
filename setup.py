f17d98382f4306786d60b8d0dc6afa037ac24c21
fajari <m.soleh.fajari@gmail.com>
2022-11-19 20:27:10 +0700

    Update 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
 
     name="IndonesiaLatestEarthQuakeAlert",
-    version="0.0.1",
+    version="0.0.2",
     author="M Soleh Fajari",
     author_email="<m.soleh.fajari@gmail.com>",
     description="This package will guide us get earth quake information from bmkg",
