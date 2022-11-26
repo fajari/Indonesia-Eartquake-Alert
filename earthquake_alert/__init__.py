96d2819170cd44a408d6d515a1c8f8c8c47acdda
fajari <m.soleh.fajari@gmail.com>
2022-11-26 15:12:43 +0700

    update magnitudo_eq2

--- earthquake_alert/__init__.py
+++ earthquake_alert/__init__.py
@@ -1,56 +1,6 @@
 from typing import List
-
 import requests
 from bs4 import BeautifulSoup
-
-"""
-def data_extraction():
-    try:
-        content = requests.get('https://www.bmkg.go.id') #scrape from bmkg website
-    except Exception:
-        return None
-
-    if content.status_code == 200: # Check if website status OK/200
-        soup = BeautifulSoup(content.text, 'html.parser')
-        result = soup.find('span', {'class': 'waktu'})
-        result = result.text.split(', ')
-        date_eq = result[0]
-        time_eq = result[1]
-
-        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
-        result = result.findChildren('li')
-        i = 0
-        magnitudo_eq = None
-        ls = None
-        bt = None
-        location_place_eq = None
-        location_eq = None
-        depth_eq = None
-
-        for res in result:
-            if i == 1:
-                magnitudo_eq = res.text
-            elif i == 2:
-                depth_eq = res.text
-            elif i == 3:
-                location_eq = res.text.split(' - ')
-                ls = location_eq[0]
-                bt = location_eq[1]
-            elif i == 4:
-                location_place_eq = res.text
-            i = i +1
-
-        scrape_result = dict()
-        scrape_result['date_eq'] = date_eq #Date of earth quake
-        scrape_result['time_eq'] = time_eq #Time of earth quake
-        scrape_result['magnitudo_eq'] = magnitudo_eq #Magnitudo of earth quake
-        scrape_result['location_place_eq'] = location_place_eq #Location of earth quake
-        scrape_result['location_eq'] = {'ls': ls, 'bt': bt} #Coordinate of earth quake
-        scrape_result['depth_eq'] = depth_eq # Depth of earth quake
-        return scrape_result
-    else:
-        return None
-"""
 def data_extraction():
     try:
         content = requests.get('https://warning.bmkg.go.id') #scrape from bmkg website
@@ -59,19 +9,22 @@ def data_extraction():
 
     if content.status_code == 200: # Check if website status OK/200
         soup = BeautifulSoup(content.text, 'html.parser')
-        result = soup.find('h5', {'class': 'center'})
-        result2 = soup.find('h5', {'class': 'center'})
-        result = result.text.split(', ')
+        result = soup.find('h5', {'class': 'center'}) #get information location
+        result2 = soup.find('h5', {'class': 'center'}) #get information location
+        result = result.text.split(', ') #devide information into date and time
         date_eq = result[0]
         time_eq = result[1]
 
-        result = soup.find('ul', {'class': 'infolindu'})
-        result2 = soup.find('div', {'class': 'infoext'})
-        result = result.findChildren('li')
-        result2 = result2.findChildren('p')
+        result = soup.find('ul', {'class': 'infolindu'}) #get information from infolindu
+        result2 = soup.find('div', {'class': 'infoext'}) #get information from infoext
+        result = result.findChildren('li') # search html li
+        result2 = result2.findChildren('p') #search html p
+
+        # describe variable
         i = 0
         x = 0
         magnitudo_eq = None
+        magnitudo_eq2 = None
         ls = None
         bt = None
         location_place_eq = None
@@ -79,6 +32,7 @@ def data_extraction():
         depth_eq = None
         impacted_area = None
 
+        # fill variable with with content of website
         for res in result:
             if i == 0:
                 magnitudo_eq = res.text.split('M')
@@ -87,11 +41,9 @@ def data_extraction():
                 depth_eq = res.text.split('K')
             elif i == 2:
                 location_eq = res.text
-            #    ls = location_eq[0]
-            #    bt = location_eq[1]
             i = i + 1
 
-
+        # fill variable with with content of website
         for res in result2:
             if x == 0:
                 location_place_eq = res.text.split('Gempa')
@@ -99,6 +51,7 @@ def data_extraction():
                 impacted_area = res.text.split('Arahan')
             x = x + 1
 
+        # save result in variable
         scrape_result = dict()
         scrape_result['date_eq'] = date_eq  # Date of earth quake
         scrape_result['time_eq'] = time_eq  # Time of earth quake
@@ -113,6 +66,7 @@ def data_extraction():
     else:
         return None
 
+# this section used to display information
 def show_data(result):
     if result is None:
         print("Can not find earth quake data, plese check target url")
